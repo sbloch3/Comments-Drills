6f8f578beffaeb9b8822e401666b9ba0a481d96b
sbloch3 <sbloch3@wisc.edu>
2021-11-21 21:31:49 -0600

    Create commentDrills.py

---
+++ commentDrills.py
@@ -0,0 +1,39 @@
+'''
+For this assignment you should read the task, then below the task do what 
+it asks you to do.
+EXAMPLE TASK:
+'''
+#EX) Make a single line comment
+#This is a single line comment
+
+'''
+END OF EXAMPLE
+'''
+
+'''
+START HERE
+'''
+
+#1) Make a single line comment.
+#this is a single line comment
+'''Sarah's single line comment'''
+
+
+#2) Make a multi-line comment.
+#this is a multi-line comment
+'''Sarah's single line comment
+is now a multi-line comment'''
+
+
+#3) Make a variable. Then put a comment after it, on the same line.
+x = 5 '''the variable x is equal to five'''
+
+
+#4) Make a single line comment.
+''' Sarah's second single line comment'''
+
+
+#5) Make a multi-line comment.
+''' Sarah's second time writing not a single line
+but a multi-line comment'''
+
